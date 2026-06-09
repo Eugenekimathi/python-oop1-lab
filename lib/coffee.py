7d1625ce076b9fc1a1800cc953a616c439ce54b6
Eugene Kimathi <eugenekimathi@gmail.com>
2026-06-09 14:03:47 +0300

    Add coffee class

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self,size,price):
+        if size in ["Small","Meduim","Large"]:
+            self._size = size 
+        else:
+            print("size must be small, Meduim, or Large")
+            self._size = None
+            self.price = price
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,value):
+        if value in ["Small","Meduim","Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        print("Here's a tip")
+        self.price += 1 
+
+# Create a coffee
+latte = Coffee("Medium", 3.5)
+latte.tip()         # Output: This coffee is great, here’s a tip!
+print(latte.price)  # 4.5                          
