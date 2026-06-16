15825f0a23fc2ba047b79b325e498d9656fb1a5f
Eugene Kimathi <eugenekimathi@gmail.com>
2026-06-16 14:15:15 +0300

    fixed error

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    def __init__(self,size,price):
-        if size in ["Small","Meduim","Large"]:
-            self._size = size 
+    def __init__(self, size, price):
+        if size in ["Small", "Medium", "Large"]:
+            self._size = size
         else:
-            print("size must be small, Meduim, or Large")
+            print("size must be Small, Medium, or Large")
             self._size = None
         self.price = price
 
     @property
     def size(self):
         return self._size
-    
+
     @size.setter
-    def size(self,value):
-        if value in ["Small","Meduim","Large"]:
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
             self._size = value
         else:
             print("size must be Small, Medium, or Large")
 
     def tip(self):
-        print(" This coffee is great, here's a tip!")
+        print("This coffee is great, here's a tip!")
         self.price += 1
 
